eeb24418b770c6a3b4465b6e8e131e43c45b3d93
sam bacha <sam@manifoldfinance.com>
2025-01-10 01:08:36 -0800

    fix(solidity): test case replace

--- crates/language/src/solidity.rs
+++ crates/language/src/solidity.rs
@@ -1,14 +1,23 @@
 #![cfg(test)]
+//! Standalone Solidity file to test syntax.
+//! Solidity does not need special processing and can be a stub lang.
+
 use ast_grep_core::source::TSParseError;
 
 use super::*;
 
-fn test_match(query: &str, source: &str) {
-  use crate::test::test_match_lang;
-  test_match_lang(query, source, Solidity);
+fn test_replace(src: &str, pattern: &str, replacer: &str) -> Result<String, TSParseError> {
+    use crate::test::test_replace_lang;
+    test_replace_lang(src, pattern, replacer, Solidity)
 }
 
-fn test_non_match(query: &str, source: &str) {
-  use crate::test::test_non_match_lang;
-  test_non_match_lang(query, source, Solidity);
+#[test]
+fn test_solidity_replace() -> Result<(), TSParseError> {
+    let ret = test_replace(
+      "token.transfer(recipient)",
+      "$CONTRACT.transfer($ADDR)",
+      "$CONTRACT.safeTransfer($ADDR)",
+    )?;
+    assert_eq!(ret, "token.safeTransfer(recipient)");
+    Ok(())
 }
